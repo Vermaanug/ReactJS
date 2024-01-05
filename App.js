9ee56036c62b9c766c8a9da391275940df6778f4
Vermaanug <Vermaanug1974@gmail.com>
2024-01-05 16:09:11 +0530

    Completed First Chapter of NamasteReact

---
+++ App.js
@@ -0,0 +1,21 @@
+const Parent = React.createElement(
+  "div",
+  { id: "Parent" },
+  React.createElement(
+    "div",
+    { id: "child" },
+    [React.createElement("h1", { } , "Hello I'm H1 tag") , React.createElement("h2", { } , "Hello I'm H2 tag")]
+  )
+);
+
+// const heading = React.createElement(
+//   "H1",
+//   { id: "heading ", Style: "color:red" },
+//   "Hello Wolrd Form Reacts"
+// );
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// console.log(heading); //  return Object
+
+root.render(Parent);
