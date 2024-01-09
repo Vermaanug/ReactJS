e2991c4aafa9756140f7b23b14ae95f373163baa
Vermaanug <Vermaanug1974@gmail.com>
2024-01-09 20:51:55 +0530

    Completed Second Lecture of NamasteReact

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+
 const Parent = React.createElement(
   "div",
   { id: "Parent" },
-  React.createElement(
-    "div",
-    { id: "child" },
-    [React.createElement("h1", { } , "Hello I'm H1 tag") , React.createElement("h2", { } , "Hello I'm H2 tag")]
-  )
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "Welocome "),
+    React.createElement("h1", {}, "Welocome "),
+    React.createElement("h1", {}, "Hello I'm H1 tag"),
+    React.createElement("h2", {}, "Hello I'm H2 tag"),
+  ])
 );
 
 // const heading = React.createElement(
